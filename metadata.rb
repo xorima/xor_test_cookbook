2a7e8c06a53d6a95599caff7faeafb6555ec896d
Jason Field <4923914+Xorima@users.noreply.github.com>
2020-10-25 17:12:18 +0000

    Update metadata for 3.5.999

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'donotuse'
 license           'Apache-2.0'
 description       'Do not use!'
 chef_version      '>= 55.0'
-version           '3.2.0'
+version           '3.5.999'
 
 %w(ubuntu redhat centos fedora).each do |os|
   supports os
